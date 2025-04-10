123004063c84dc2d82f1a2a788004402f5fd9feb
TimofeevKLim <karelian_company@mail.ru>
2025-04-10 03:27:35 +0300

    10.04.2025//Создал главное окно. Убрав встроенные параметры окна, сделал собственную панель управления окном и добавил возможность изменять его размер. Далее буду верстать страницы приложения и добавлять в него разработанный ранее интерфейс прослушивания и редактирования музыки

--- Mediaplayer2/Views/MainWindow.axaml.cs
+++ Mediaplayer2/Views/MainWindow.axaml.cs
@@ -6,28 +6,48 @@ namespace Mediaplayer2.Views;
 
 public partial class MainWindow : Window
 {
+    // Переменная для кнопок развернуть/вернуть прежний размер
     private bool _boobool;
     public MainWindow()
     {
         InitializeComponent();
     }
-
+    // Метод для перемещения окна по экрану с помощью верхней панели
     private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
         BeginMoveDrag(e);
     }
-
+    // Метод для добавления возможности изменять размеры окна
+    void SetupSide(string name, StandardCursorType cursor, WindowEdge edge)
+    {
+        var ctl = this.Get<Control>(name);
+        ctl.Cursor = new Cursor(cursor);
+        ctl.PointerPressed += (i, e) =>
+        {
+            BeginResizeDrag(edge, e);
+        };
+    }
+    // Метод инициализации, в котором задаются функции управления окном
     private void InitializeComponent()
     {
+        // 
         AvaloniaXamlLoader.Load(this);
+        // Кнопки управления окном (свернуть/развернуть/вернуть прежний размер/закрыть)
         this.Get<Button>("MinimizeWindow").Click += delegate { this.WindowState = WindowState.Minimized; };
         this.Get<Button>("MaximizeWindow").Click += delegate { this.WindowState = WindowState.Maximized; _boobool = true; MaxMinButton(); };
         this.Get<Button>("CloseWindow").Click += delegate { this.Close(); };
         this.Get<Button>("NormalWindow").Click += delegate { this.WindowState = WindowState.Normal; _boobool = false; MaxMinButton(); };
-
-        
+        // Добавление возможности изменять размеры окна
+        SetupSide("Left", StandardCursorType.LeftSide, WindowEdge.West);
+        SetupSide("Right", StandardCursorType.RightSide, WindowEdge.East);
+        SetupSide("Top", StandardCursorType.TopSide, WindowEdge.North);
+        SetupSide("Bottom", StandardCursorType.BottomSide, WindowEdge.South);
+        SetupSide("TopLeft", StandardCursorType.TopLeftCorner, WindowEdge.NorthWest);
+        SetupSide("TopRight", StandardCursorType.TopRightCorner, WindowEdge.NorthEast);
+        SetupSide("BottomLeft", StandardCursorType.BottomLeftCorner, WindowEdge.SouthWest);
+        SetupSide("BottomRight", StandardCursorType.BottomRightCorner, WindowEdge.SouthEast);
     }
-
+    // Метод для кнопок развернуть/вернуть прежний размер
     private void MaxMinButton()
     {
         if (_boobool) //???
--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b32e1a5874bd0cb499606a90d9755fd21e3f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
