e5d2b08637ec55e817280a9fa49337e0ec16c596
TimofeevKLim <karelian_company@mail.ru>
2025-04-13 14:57:18 +0300

    13.04.2025//Правлю разметку главной страницы

--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344b13fdfd3b45903b1e8064a53539e1dd6bd602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f317074db264a199f638f1be7332067c2fead03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
