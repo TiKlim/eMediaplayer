a657ea9f74ede23e25f6855fc275a9bbae62899e
TimofeevKLim <karelian_company@mail.ru>
2025-04-22 09:51:00 +0300

    22.04.2025//Незначительные изменения

--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0533f8c80edac010e712742ad9322384d7ae5aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57cee0269f18922bab017f29213ee29f72d3ea86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
