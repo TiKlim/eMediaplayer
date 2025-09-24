591f331357589640d9624b9280d17e57697b17ec
TiKlim <karelian_company@mail.ru>
2025-09-24 19:42:50 +0300

    refactor: добавил значок приложения в навигационную панель

--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d404b9ceaa3ba6e93ec7f6402b1eb0cc436e70b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329f455f7e7882baa139deae471a074b22ef1713")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
