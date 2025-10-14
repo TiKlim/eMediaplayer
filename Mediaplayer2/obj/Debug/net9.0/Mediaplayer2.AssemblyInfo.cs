306360677731c0c623f7c9ab10a78b0dac819bb1
TiKlim <karelian_company@mail.ru>
2025-10-14 21:32:32 +0300

    refactor: незначительные изменения

--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0dd7d8c75b49ad13d129039382feb45d98303e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50a1d188bdaebb0fc14e5c5453e1aa5f2b15fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
