ea0261ca0da4924aba7534019d8ccd02c0de70af
TiKlim <karelian_company@mail.ru>
2025-11-11 16:28:20 +0300

    refactor: незначительные изменения

--- LibVLCSharp.Avalonia.Unofficial/obj/Debug/net9.0/LibVLCSharp.Avalonia.Unofficial.AssemblyInfo.cs
+++ LibVLCSharp.Avalonia.Unofficial/obj/Debug/net9.0/LibVLCSharp.Avalonia.Unofficial.AssemblyInfo.cs
@@ -22,7 +22,7 @@ This package depends on LibVLCSharp as well as Avalonia.
 LibVLC needs to be installed separately, see VideoLAN.LibVLC.* packages.
     ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a289b829fc4cd1258377aec610625351857fb8b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622c1e915d762bed452ec4f0b1123c979a3596f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibVLCSharp.Avalonia.Unofficial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibVLCSharp.Avalonia.Unofficial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a289b829fc4cd1258377aec610625351857fb8b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622c1e915d762bed452ec4f0b1123c979a3596f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
