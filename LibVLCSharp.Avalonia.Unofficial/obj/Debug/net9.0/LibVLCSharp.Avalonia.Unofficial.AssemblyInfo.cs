aa9fb54cd4e33534ae08a07f30a82ec3ad538427
TiKlim <karelian_company@mail.ru>
2025-11-14 20:41:28 +0300

    refactor: незначительные изменения

--- LibVLCSharp.Avalonia.Unofficial/obj/Debug/net9.0/LibVLCSharp.Avalonia.Unofficial.AssemblyInfo.cs
+++ LibVLCSharp.Avalonia.Unofficial/obj/Debug/net9.0/LibVLCSharp.Avalonia.Unofficial.AssemblyInfo.cs
@@ -22,7 +22,7 @@ This package depends on LibVLCSharp as well as Avalonia.
 LibVLC needs to be installed separately, see VideoLAN.LibVLC.* packages.
     ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0261ca0da4924aba7534019d8ccd02c0de70af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7166e90271319f299e5a58597f543e15f196d350")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibVLCSharp.Avalonia.Unofficial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibVLCSharp.Avalonia.Unofficial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
