7e02e743219aff9534417529af871299515ae5d9
TiKlim <karelian_company@mail.ru>
2025-11-19 12:50:53 +0300

    refactor: changes to LibVLCSharp.Avalonia.Unnoficial

--- Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
+++ Mediaplayer2/obj/Debug/net9.0/Mediaplayer2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e9fae7863ecadc754cdf931e642f453d1780b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfae5b422e384c8d8e80e050cc19bc7599f4d40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mediaplayer2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
