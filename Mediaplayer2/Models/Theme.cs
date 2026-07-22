b904799fa3693358ee2b27db9406029ab25a9879
TiKlim <karelian_company@mail.ru>
2026-07-22 15:11:48 +0300

    refactor: remove unnecessary assets and replace some of the remaining ones

--- Mediaplayer2/Models/Theme.cs
+++ Mediaplayer2/Models/Theme.cs
@@ -10,20 +10,10 @@ public class Theme
     public string SecondaryColor { get; set; }
     public string ThirdColor { get; set; }
     public string FourthColor { get; set; }
-    public string HomeButton { get; set; }
-    public string MusicButton { get; set; }
-    public string VideoButton { get; set; }
-    public string PlaylistButton { get; set; }
-    public string SettingsButton { get; set; }
     public string PlayButton { get; set; }
     public string StopButton { get; set; }
     public string BackTenButton { get; set; }
     public string ForeTenButton { get; set; }
-    public string BackwardButton { get; set; }
-    public string ForewardButton { get; set; }
-    public string EqualizerButton { get; set; }
-    public string EditAudioButton { get; set; }
-    public string EditVideoButton { get; set; }
     public string VolumeOnButton { get; set; }
     public string VolumeOffButton { get; set; }
     public string ListStopBack { get; set; }
